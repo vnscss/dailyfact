be343875ca1b41d73035b9299200d8422430a8fe
Vinícius Alves Pires <viniccius.alves.pires@gmail.com>
2025-09-14 19:53:34 -0300

    Refactor: melhorando estrutura do script, separando em functions e switch case para modeValue

--- dailyfact.js
+++ dailyfact.js
@@ -4,54 +4,191 @@ import {GoogleGenAI,Type,} from '@google/genai';
 import { JSDOM }  from "jsdom";
 import * as fs from "fs";
 import { fileURLToPath } from 'url';
-
 import { dirname , join } from 'path';
-
 
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 dotenv.config({ path: join(__dirname, ".env") });
 
-console.clear();
-
-let args = process.argv.slice(2);
-
-let dValue = null;
-let mValue = null;
-let modeValue = null;
-
-for (let i = 0; i < args.length; i++) {
-    if (args[i] === "-d") {
-        dValue = Number(args[i + 1]);
-        i++; // pula o próximo porque já pegamos o valor
-    } else if (args[i] === "-m") {
-        mValue = args[i + 1];
-        i++;
-    }else if (args[i] === "-mode") {
-        modeValue = Number(args[i + 1]);
-        i++;
-    }
-}
-
 
 let data = new Date();
 let month = data.toLocaleString("pt" , { month: 'long'});
 let day = data.getDate();
 
-if (dValue) day = dValue;
-if (mValue) month = mValue;
-
-
-
 const apiKey = process.env.GOOGLE_SEARCH_API;
 const cx = process.env.GOOGLE_CX_ID;
 let google_search_string = `"em ${day} de ${month}" (tecnologia OR ciência OR inovação OR software OR hardware OR internet OR cibersegurança) site:tecmundo.com.br OR site:canaltech.com.br OR site:olhardigital.com.br OR site:showmetech.com.br OR site:theverge.com OR site:wired.com OR site:engadget.com OR site:techradar.com OR site:arstechnica.com`
 
-
 let facts = [];
+let best_facts = [];
 
 
+const main = async () => {
+
+    let args = process.argv.slice(2);
+
+    let dValue = null;
+    let mValue = null;
+    let modeValue = null;
+
+    for (let i = 0; i < args.length; i++) {
+        if (args[i] === "-d") {
+            dValue = Number(args[i + 1]);
+            i++; // pula o próximo porque já pegamos o valor
+        } else if (args[i] === "-m") {
+            mValue = args[i + 1];
+            i++;
+        }else if (args[i] === "-mode") {
+            modeValue = Number(args[i + 1]);
+            i++;
+        }
+    }
+
+
+    if (dValue) day = dValue;
+    if (mValue) month = mValue;
+
+    //Fatos da wiki
+    let wiki_url = `https://pt.wikipedia.org/wiki/${day}_de_${month}`;
+    loadingAnimation('Pegando fatos da wiki', 'start');
+    let wiki_document = await fetch(wiki_url).then(res => res.text());
+    
+    
+    let dom = new JSDOM(wiki_document);
+    let document = dom.window.document;
+    
+    let ul = document.querySelector("ul:not([class]):not([id])");
+    
+    let lis = Array.from(ul.children); 
+    
+    lis.forEach(li => {
+        if (li.tagName === "LI") {
+            facts.push({
+                title: null,
+                link: wiki_url,
+                snippet: li.textContent
+            });
+        }
+    });
+    loadingAnimation('', 'stop');
+    
+    
+    loadingAnimation('Pegando fatos do google', 'start');
+    
+    //Fatos da do google
+    let customsearch = google.customsearch('v1');
+    try {
+    const res = await customsearch.cse.list({
+        auth: apiKey,
+        cx: cx,
+        q: google_search_string,
+        num: 10
+    });
+    
+    const items = res.data.items;
+    if (items) {
+        items.forEach((item, index) => {
+            let fact = {
+                title: item.title,
+                snippet: item.snippet,
+                link: item.link
+            }
+            facts.push(fact)
+        });
+    } 
+    } catch (err) {
+    }
+    
+    loadingAnimation('', 'stop');
+
+    facts.forEach((fact, index) => {
+      fact.index = index; 
+    });
+
+    let selectBest_promptPath
+    let format_prompPath
+
+
+    let index_responseSchema = {
+      type: Type.OBJECT,
+      properties: {
+        response: {
+          type: Type.ARRAY,
+          items: {
+            type: Type.INTEGER,
+            },
+          },
+        },
+      }
+
+    let string_responseSchema = {
+      type: Type.OBJECT,
+      properties: {
+        response: {
+          type: Type.ARRAY,
+          items: {
+            type: Type.OBJECT,
+            properties: {
+              text: { type: Type.STRING },
+              index: { type: Type.INTEGER }
+            }
+          }
+        }
+      }
+    }
+
+    switch (modeValue) {
+      case 2:
+        selectBest_promptPath = `${__dirname}/prompts/select_3_best_fact_system_instructions.txt`;
+        format_prompPath = `${__dirname}/prompts/format_3_facts_system_instructions.txt`;
+        break;
+
+      case 3:
+        console.log(facts)
+        selectBest_promptPath = `${__dirname}/prompts/select_3_best_fact_system_instructions.txt`;
+        format_prompPath = `${__dirname}/prompts/format_3_facts_system_instructions.txt`;
+        break;
+
+      case null:
+        selectBest_promptPath = `${__dirname}/prompts/select_best_fact_system_instructions.txt`;
+        format_prompPath = `${__dirname}/prompts/format_fact_system_instructions.txt`;
+        break;
+
+    }
+
+
+    let system_instructions = readPrompt(selectBest_promptPath);
+    let prompt = JSON.stringify(facts)
+
+    loadingAnimation('Pegando o melhor fato', 'start');
+    let geminiResponse = await gemini(system_instructions , prompt , index_responseSchema);
+    loadingAnimation('', 'stop');
+
+    let best_index = formatAsJson(geminiResponse)
+
+    best_index.response.map(index => {
+      best_facts.push(facts[index]);
+    });
+
+    system_instructions = readPrompt(format_prompPath);
+    prompt = JSON.stringify(best_facts)
+
+    let fromated_response
+
+    loadingAnimation('Formatando resposta', 'start');
+    geminiResponse = await gemini(system_instructions , prompt , string_responseSchema);
+    loadingAnimation('', 'stop');
+    
+    fromated_response = formatAsJson(geminiResponse);
+    fromated_response = fromated_response.response;
+
+    fromated_response.map(fact => {
+      drawFact(fact);
+    });
+
+
+}
 
 
 let loadingInterval;
@@ -79,67 +216,15 @@ function loadingAnimation(message, action) {
     }
 }
 
+function readPrompt(filePath) {
+  let content = fs.readFileSync(filePath, "utf8");
 
+  content = content
+    .replace(/\$\{day\}/g, day)
+    .replace(/\$\{month\}/g, month);
 
-
-
-//Fatos da wiki
-
-let wiki_url = `https://pt.wikipedia.org/wiki/${day}_de_${month}`;
-loadingAnimation('Pegando fatos da wiki', 'start');
-let wiki_document = await fetch(wiki_url).then(res => res.text());
-
-
-let dom = new JSDOM(wiki_document);
-let document = dom.window.document;
-
-let ul = document.querySelector("ul:not([class]):not([id])");
-
-let lis = Array.from(ul.children); 
-
-lis.forEach(li => {
-    if (li.tagName === "LI") {
-        facts.push({
-            title: null,
-            link: null,
-            snippet: li.textContent
-        });
-    }
-});
-loadingAnimation('', 'stop');
-
-let customsearch = google.customsearch('v1');
-
-
-loadingAnimation('Pegando fatos do google', 'start');
-try {
-const res = await customsearch.cse.list({
-    auth: apiKey,
-    cx: cx,
-    q: google_search_string,
-    num: 10
-});
-
-const items = res.data.items;
-if (items) {
-    items.forEach((item, index) => {
-        let fact = {
-            title: item.title,
-            snippet: item.snippet,
-            link: item.link
-        }
-        facts.push(fact)
-    });
-} 
-} catch (err) {
+  return content;
 }
-
-loadingAnimation('', 'stop');
-facts.forEach((fact, index) => {
-  fact.index = index; 
-});
-
-
 
 async function gemini(system_instructions , promptString , responseSchema){
 
@@ -186,154 +271,25 @@ async function gemini(system_instructions , promptString , responseSchema){
     return fullText;
 }
 
-let filePath 
-let index_responseSchema 
+function formatAsJson(string){
+  let response 
 
-if(modeValue === 2){
-  filePath = `${__dirname}/prompts/select_3_best_fact_system_instructions.txt`
-  index_responseSchema = {
-      type: Type.OBJECT,
-      properties: {
-        index: {
-          type: Type.ARRAY,
-          items: {
-            type: Type.INTEGER,
-            },
-          },
-        },
-      }
-}
-else{
-  filePath = `${__dirname}/prompts/select_best_fact_system_instructions.txt`;
+  response = string.replace("undefined" , "")
+  .replace("```" , "").replace('json' , '')
 
-  index_responseSchema = {
-      type: Type.OBJECT,
-      properties: {
-        index: {
-          type: Type.NUMBER,
-        },
-      },
-    }
+  response = JSON.parse(response)
+
+  return response
 }
 
-let content = fs.readFileSync(filePath, "utf8");
-content = content
-  .replace(/\$\{day\}/g, day)
-  .replace(/\$\{month\}/g, month);
+function drawFact(obj){
 
-let select_best_fact_system_instructions = content
+  let text = obj.text
+  let fonte = facts?.[obj.index].link;
 
-
-loadingAnimation('Separando melhor fato', 'start');
-let geminiResponse = await gemini(select_best_fact_system_instructions , JSON.stringify(facts) , index_responseSchema)
-
-let obj = JSON.parse(geminiResponse.replace("undefined" , "")); 
-
-let best_fact 
-let string_responseSchema 
-if(modeValue === 2){
-
-  let best_facts = []
-  obj.index.map( index => {
-    best_facts.push(facts[index])
-  })
-  
-  best_fact = best_facts
-
-  filePath = `${__dirname}/prompts/format_3_facts_system_instructions.txt`;
-
-  string_responseSchema =  {
-  type: Type.OBJECT,
-  properties: {
-    response: {
-      type: Type.ARRAY,
-      items: {
-        type: Type.OBJECT,
-        properties: {
-          text: { type: Type.STRING },
-          index: { type: Type.INTEGER }
-        }
-      }
-    }
-  }
-}
-
-
-
-
-}
-else{
-  best_fact = facts[obj.index]
-
-  filePath = `${__dirname}/prompts/format_fact_system_instructions.txt`;
-
-  string_responseSchema =  {
-      type: Type.OBJECT,
-      properties: {
-        response: {
-          type: Type.STRING,
-        },
-      },
-    }
-}
-
-loadingAnimation('', 'stop');
-
-
-content = fs.readFileSync(filePath, "utf8");
-content = content
-  .replace(/\$\{day\}/g, day)
-  .replace(/\$\{month\}/g, month);
-
-let format_fact_system_instructions = content
-
-loadingAnimation('Formatando resposta', 'start');
-let finalGeminiResponse = await gemini(format_fact_system_instructions , JSON.stringify(best_fact) , string_responseSchema)
-
-
-finalGeminiResponse = JSON.parse(finalGeminiResponse.replace("undefined" , "")); 
-
-loadingAnimation('', 'stop');
-
-if(modeValue === 2){
-
-  finalGeminiResponse = finalGeminiResponse.response;
-
-
-  console.clear();
-
-  finalGeminiResponse.map( fact =>{
-    let text = fact.text
-    let index = fact.index
-
-    console.log("\n\x1b[1;34m===== Curiosidade do Dia =====\x1b[0m"); // título azul e em negrito
-    console.log(`\x1b[1m${text}\x1b[0m`); // snippet em negrito
-
-    let fonte = null
-
-    if(best_fact[index]?.link){
-      fonte = best_fact[index].link
-
-      console.log(`\x1b[36mFonte:\x1b[0m ${fonte}`); // 'Fonte:' ciano
-    }
-    else{
-       console.log(`\x1b[36mFonte:\x1b[0m https://www.wikipedia.org/`); // 'Fonte:' ciano
-    }
-
-    console.log("\x1b[1;34m============================\x1b[0m\n"); // linha azul
-
-
-  })
-}
-else{
-
-  finalGeminiResponse = finalGeminiResponse.response;
-  let fonte = best_fact.link;
-
-  console.clear();
 
   console.log("\n\x1b[1;34m===== Curiosidade do Dia =====\x1b[0m"); // título azul e em negrito
-  console.log(`\x1b[1m${finalGeminiResponse}\x1b[0m`); // snippet em negrito
+  console.log(`\x1b[1m${text}\x1b[0m`); // snippet em negrito
 
   if(fonte){
       console.log(`\x1b[36mFonte:\x1b[0m ${fonte}`); // 'Fonte:' ciano
@@ -344,5 +300,6 @@ else{
 
   console.log("\x1b[1;34m============================\x1b[0m\n"); // linha azul
 
-
 }
+
+await main();
