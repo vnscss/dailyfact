9ec7ea496779573bb1db0f26c86784b7cba8f616
Vinícius Alves Pires <viniccius.alves.pires@gmail.com>
2025-09-14 22:27:05 -0300

    Feat: add data para -dev output

--- dailyfact.js
+++ dailyfact.js
@@ -205,7 +205,8 @@ const main = async () => {
 
           let fato = {
             text: text,
-            fonte: fonte
+            fonte: fonte,
+            data: data
           }
 
           facts_JSON.push(fato)
